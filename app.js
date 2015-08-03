4d2e9e7e74f44e49385946be2e9aabca5b13c3c9
VKravchuk <VKravchuk94@gmail.com>
2015-08-03 18:46:53 +0300

    connect redis, get cookie, write test cookie

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+var redis = require("redis");
+var client = redis.createClient();
+var cookieParser = require('cookie')
+var PHPUnserialize = require('php-unserialize');
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
+
 
 // Routing
 app.use(express.static(__dirname + '/public'));
@@ -19,64 +24,99 @@ var usernames = {};
 var numUsers = 0;
 
 io.on('connection', function (socket) {
-  var addedUser = false;
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    console.log('username', socket.username);
-    console.log('message', data);
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    // we store the username in the socket session for this client
-    console.log('username', username)
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    // remove the username from global usernames list
-    if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
+    var addedUser = false;
+    if(checkSession(socket.request.headers.cookie)){
+        console.log(socket.request.headers.cookie);
     }
-  });
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        console.log('username', socket.username);
+        console.log('message', data);
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        console.log('username', username)
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        // remove the username from global usernames list
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
 });
+
+
+function checkSession(cookie) {
+    var parsedCookie = cookieParser.parse(cookie);
+    var sessionConfig = {
+        client: {
+            key: 'test_key'
+        },
+        redis: {
+            prefix: 'test_redis_key_'
+        }
+    };
+    var sessionKey = sessionConfig.redis.prefix + parsedCookie[sessionConfig.client.key];
+    var isLogin = false;
+    client.get(sessionKey, function (err, replies) {
+        if (err) {
+            console.log(err);
+            isLogin = false;
+        }
+        else if (replies) {
+            console.log(replies.substring(1, replies.length - 1).replace(/\\/g, ''));
+            console.log(PHPUnserialize.unserialize(replies.substring(1, replies.length - 1).replace(/\\/g, '')));
+            isLogin = true;
+        }
+        else {
+            console.log('Record does not exist!');
+            isLogin = false;
+        }
+        client.quit();
+    });
+    return isLogin;
+}
--- public/scripts/main.jsx
+++ public/scripts/main.jsx
@@ -1,5 +1,5 @@
 var ChatWrapper = require('./components/ChatWrapper.jsx');
-
+document.cookie = 'test_key="test_value"';
 React.render(
 <ChatWrapper/>,
     document.getElementById('content')
